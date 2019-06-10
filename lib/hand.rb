bdc1f2dc47274f77628b016a328a24925c6b4cf4
Tatiana Morozova <tabobo@mail.ru>
2019-06-10 14:38:04 +0300

    methods

---
+++ lib/hand.rb
@@ -0,0 +1,29 @@
+class Hand
+  CARDS_MAX_AMOUNT = 3
+
+  attr_reader :card, :deck, :cards
+
+  def initialize(cards)
+    @cards = []
+  end
+
+  def take_card(deck)
+    card = deck.random_card
+    deck.delete_card(card)
+    new_card(card)
+  end
+
+  def new_card(card)
+    @cards << card
+  end
+
+  def open_cards
+  end
+
+  def hand_value
+    points = 0
+    @cards.each { |card| points += card.value}
+    return points
+  end
+
+end
